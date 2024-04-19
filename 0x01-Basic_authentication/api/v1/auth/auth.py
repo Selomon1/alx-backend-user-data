bbcd19258789e4f8d8dbe9500c1986ebe34312c7
Selomon1 <soya.vandy1950@gmail.com>
2024-04-19 22:37:07 +0300

    Basic Authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -30,9 +30,10 @@ class Auth:
                 excluded_path += '/'
             if excluded_path.endswith('*'):
                 if path.startswith(excluded_path[:-1]):
-                    return false
+                    return False
             elif path == excluded_path:
                 return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
