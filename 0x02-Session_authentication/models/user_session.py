788b9bbdec2fee65e2f1ae62f959d4c0f101e250
Selomon1 <soya.vandy1950@gmail.com>
2024-04-21 01:43:44 +0300

    Session Authentication

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-"""
-Module of User Sessions
-"""
+"""Module of User Sessions."""
 
 from .base import Base
 
 
 class UserSession(Base):
-    """ class for storing user sessions """
+    """Class for storing user sessions."""
 
     def __init__(self, *args: list, **kwargs: dict):
-        """ Initialize UserSession instance """
+        """Initialize UserSession instance."""
         super().__init__(*args, **kwargs)
         self.user_id = kwargs.get("user_id")
         self.session_id = kwargs.get("session_id")
