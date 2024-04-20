a185f2271e39766472729498799d9262519689c1
Selomon1 <soya.vandy1950@gmail.com>
2024-04-21 01:12:52 +0300

    Session Authentication

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -5,8 +5,6 @@ Module
 
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 from os import getenv
 
 
