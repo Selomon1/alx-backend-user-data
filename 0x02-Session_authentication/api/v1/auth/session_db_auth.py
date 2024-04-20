32cbc83d4eda3d59ce342138c9e36d1888dfa87e
Selomon1 <soya.vandy1950@gmail.com>
2024-04-21 02:38:30 +0300

    Session Authentication

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -4,6 +4,7 @@
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
 import uuid
+from datetime import datetime, timedelta
 
 
 class SessionDBAuth(SessionExpAuth):
@@ -11,28 +12,51 @@ class SessionDBAuth(SessionExpAuth):
 
     def create_session(self, user_id=None):
         """Create a session and store it in the database"""
-        if user_id:
-            session_id = str(uuid.uuid4())
-            new_session = UserSession(
-                user_id=user_id,
-                session_id=super().create_session(user_id)
-            )
-            new_session.save()
-            return session_id
-        else:
+        session_id = super().create_session(user_id)
+        if not session_id:
             return None
+
+        session_id = str(uuid.uuid4())
+        user_session = UserSession(
+            user_id=user_id,
+            session_id=session_id
+        )
+        user_session.save()
+        return session_id
 
     def user_id_for_session_id(self, session_id=None):
         """Retrieve user id from the database for a given session"""
-        if session_id is None:
+        if not session_id or not isinstance(session_id, str):
             return None
+
         user_session = UserSession.get(session_id)
-        if user_session:
-            return user_session.user_id
-        return None
+        if not user_session:
+            return None
+
+        session_expiry = (
+            user_session.created_at +
+            timedelta(seconds=self.session_duration)
+        )
+        if session_expiry < datetime.utcnow():
+            user_session.remove()
+            return none
+
+        return user_session.user_id
 
     def destroy_session(self, request=None):
         """Destroy the session stored in the database"""
-        session_id = self.cookies.get(self.session_name)
-        if session_id:
-            UserSession.delete(session_id=session_id)
+        if not request:
+            return False
+
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return False
+
+        user_id = self.user_id_for_session_id(session_id)
+        if not user_id:
+            return false
+
+        user_session = UserSession.get(session_id)
+        user_sesssion.remove()
+
+        return True
