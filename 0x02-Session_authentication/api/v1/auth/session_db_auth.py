1adda60bdc9efb712c409c7b7675c9cc9c95d906
Selomon1 <soya.vandy1950@gmail.com>
2024-04-21 01:52:35 +0300

    Session Authentication

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-Module SessionDB Authentication
-"""
+"""Module SessionDB Authentication"""
 
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
@@ -9,25 +7,21 @@ import uuid
 
 
 class SessionDBAuth(SessionExpAuth):
-    """
-    Session database authentication class
-    """
+    """Session database authentication class"""
 
     def create_session(self, user_id=None):
-        """ Create a session and store it in the database """
-        session_id = super().create_session(user_id)
-        if session_id:
-            session_id = str(uuid.uuid4())
-            new_session = UserSession(
-                user_id=user_id,
-                session_id=session_id)
-            )
-            new_session.save()
-            return session_id
-        return None
+        """Create a session and store it in the database"""
+        session_id = str(uuid.uuid4())
+        new_session = UserSession(
+            user_id=user_id,
+            session_id=super().create_session(user_id)
+        )
+        new_session.save()
+
+        return session_id
 
     def user_id_for_session_id(self, session_id=None):
-        """ Retrieve user id from the database for a given session """
+        """Retrieve user id from the database for a given session"""
         if session_id is None:
             return None
         user_session = UserSession.get(session_id)
@@ -36,10 +30,9 @@ class SessionDBAuth(SessionExpAuth):
         return None
 
     def destroy_session(self, request=None):
-        """ Destroy the session stored in the database """
+        """Destroy the session stored in the database"""
         session_id = self.session_cookie(request)
         if session_id:
             user_session = UserSession.get(session_id)
             if user_session:
                 user_session.delete()
-        return None
