225b13a98bed0b5617b25bb05a0959811a3cd78c
Selomon1 <soya.vandy1950@gmail.com>
2024-04-23 11:07:03 +0300

    User Authentication

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 """DB module
 """
-import logging
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
 from user import Base, User
-
-logging.basicConfig(level=logging.WARNING)
+from typing import TypeVar
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class DB:
@@ -19,7 +18,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -46,3 +45,45 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Find a user by arbitrary keyword arguments.
+        Args:
+            **kwargs: arbitrary keyword arguments to filter users.
+        Returns:
+            User: the first user object matching the filter.
+        Raises:
+            NoResultFound: no user is founf matching the filter
+            InvalidRequestError: wrong query.
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound
+            return user
+        except InvalidRequestError:
+            raise
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        Update a user's attribute in the database
+        Args:
+            user_id (int): the ID of the user to update
+            **kwargs: arbitrary keyword arguments torepresent user attribute
+        Raises:
+            ValueError: an invalid user attribute is provided.
+        """
+        try:
+            user = self.find_user_by(id=user_id)
+        except NoResultFound:
+            raise ValueError("User not found")
+
+        for key in kwargs:
+            if not hasattr(user, key):
+                raise ValueError(f"Invalid user attribute: {key}")
+
+        for key, value in kwargs.items():
+            setattr(user, key, value)
+
+        self._session.commit()
